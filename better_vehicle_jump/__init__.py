8f7171126b70c2aaab8395dd1db3d8058cdc6169
FreepDryer <34213542+FreepDryer@users.noreply.github.com>
2026-03-12 17:38:39 -0500

    update default value for impulse

--- better_vehicle_jump/__init__.py
+++ better_vehicle_jump/__init__.py
@@ -7,8 +7,8 @@ from unrealsdk.unreal import BoundFunction, UObject, WrappedStruct
 
 vehicle_jump_height : SliderOption = SliderOption("Vehicle Jump Height", 330.0, 0.0,1000, 0.1, False, description="How high should the vehicle jump? (default is 165.0)") 
 vehicle_gravity_scalar : SliderOption = SliderOption("Vehicle Gravity Scale", 1.75, 1.0, 20.0, 0.1, False, description="Gravity multiplier for vehicles (default is 8.0)")
-vehicle_jump_impulse_distance : SliderOption = SliderOption("Vehicle Jump Impulse Distance", 500.0, 1.0, 1000.0, 0.1, False, description="Vehicle Jump Impulse Distance (default 500.0)")
-vehicle_jump_impulse_magnitude : SliderOption = SliderOption("Vehicle Jump Impulse Magnitude", 100.0, 1.0, 1000.0, 0.1, False, description="Vehicle Jump Impulse Magnitude (default is 100.0)")
+vehicle_jump_impulse_distance : SliderOption = SliderOption("Vehicle Jump Impulse Distance", 100.0, 1.0, 1000.0, 0.1, False, description="Vehicle Jump Impulse Distance (default 500.0)")
+vehicle_jump_impulse_magnitude : SliderOption = SliderOption("Vehicle Jump Impulse Magnitude", 500.0, 1.0, 1000.0, 0.1, False, description="Vehicle Jump Impulse Magnitude (default is 100.0)")
 
 @hook("/Script/OakGame.OakUIDataCollector_EquippedGadget:OnPossessPawnChanged", Type.POST)
 def Vehicle_On_Construct(obj: UObject, args: WrappedStruct, ret: Any, func: BoundFunction) -> None:
